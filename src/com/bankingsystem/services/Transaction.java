0ce0a7b245d245cfbbe9110c160925d85d8eda6d
S3M9F9 <alexandra_damgaard@swissre.com>
2024-09-27 19:28:01 +0200

    feat: Create abstract Transaction class

---
+++ src/com/bankingsystem/services/Transaction.java
@@ -0,0 +1,30 @@
+package com.bankingsystem.services;
+
+import java.time.LocalDateTime;
+
+public abstract class Transaction {
+    protected String transactionId;
+    protected double amount;
+    protected LocalDateTime timestamp;
+
+    public Transaction(String transactionId, double amount) {
+        this.transactionId = transactionId;
+        this.amount = amount;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public abstract void execute();
+
+    // Getters
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+}
