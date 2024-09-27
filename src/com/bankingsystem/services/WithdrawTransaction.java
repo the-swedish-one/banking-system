56dc16cc50a5eea89033ac0fae16a0b312380802
S3M9F9 <alexandra_damgaard@swissre.com>
2024-09-27 19:28:56 +0200

    feat: Create WithdrawTransaction subclass with overridden execute method

---
+++ src/com/bankingsystem/services/WithdrawTransaction.java
@@ -0,0 +1,28 @@
+package com.bankingsystem.services;
+
+import com.bankingsystem.models.Account;
+
+public class WithdrawTransaction extends Transaction {
+
+    private Account account;
+
+    public WithdrawTransaction(String transactionId, double amount, Account account) {
+        super(transactionId, amount);
+        this.account = account;
+    }
+
+    @Override
+    public void execute() {
+        try {
+            account.withdraw(amount);
+            account.getTransactionHistory().add(this);
+            System.out.println("Success! Amount withdrawn: " + amount);
+        } catch (Exception e) {
+            System.out.println("Withdrawal failed: " + e.getMessage());
+        }
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+}
