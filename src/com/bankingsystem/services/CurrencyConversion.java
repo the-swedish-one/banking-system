6d0470a680fd4211dc84753e4a9578e90ed9d0da
S3M9F9 <alexandra_damgaard@swissre.com>
2024-09-27 19:49:33 +0200

    feat: Create initial CurrencyConversion class with method to convert one currency to another

---
+++ src/com/bankingsystem/services/CurrencyConversion.java
@@ -0,0 +1,49 @@
+package com.bankingsystem.services;
+
+import com.bankingsystem.models.CurrencyCode;
+
+import java.util.Date;
+import java.util.Map;
+
+public class CurrencyConversion {
+    private Date lastUpdated;
+    private Map<CurrencyCode, Double> exchangeRates;
+
+    public CurrencyConversion(Map<CurrencyCode, Double> initialRates) {
+        this.exchangeRates = initialRates;
+        this.lastUpdated = new Date();
+    }
+
+    // Get the exchange rate between two currencies
+    public double getExchangeRate(CurrencyCode fromCurrency, CurrencyCode toCurrency) {
+        if (fromCurrency == toCurrency) {
+            return 1.0;
+        }
+
+        Double fromRate = exchangeRates.get(fromCurrency);
+        Double toRate = exchangeRates.get(toCurrency);
+
+        if (fromRate == null || toRate == null) {
+            throw new IllegalArgumentException("Unsupported currency conversion");
+        }
+
+        return toRate / fromRate;
+    }
+
+    // Convert a given amount from one currency to another
+    public double convertAmount(double amount, CurrencyCode fromCurrency, CurrencyCode toCurrency) {
+        double rate = getExchangeRate(fromCurrency, toCurrency);
+        return amount * rate;
+    }
+
+    // Update the exchange rates with new values and update the timestamp
+    public void updateExchangeRates(Map<CurrencyCode, Double> newRates) {
+        this.exchangeRates = newRates;
+        this.lastUpdated = new Date();
+    }
+
+    // Get the date when exchange rates were last updated
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+}
