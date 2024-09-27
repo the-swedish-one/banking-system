e6ddc84f9e1de9252ecab5d01f59c7e3ea6ec195
S3M9F9 <alexandra_damgaard@swissre.com>
2024-09-27 19:35:20 +0200

    feat: add new users and accounts to respective lists

--- src/com/bankingsystem/services/Bank.java
+++ src/com/bankingsystem/services/Bank.java
@@ -14,12 +14,14 @@ public class Bank {
 
     public void createUser(String userId, UserType userType, String name, String email, String addressLine1, String addressLine2, String city, String country) {
         User user = new User(userId, userType, name, email, addressLine1, addressLine2, city, country);
-        users.add(user);
+        this.users.add(user);
     }
 
     public void createAccount(int accountId, String IBAN, User owner1, User owner2, double balance, CurrencyCode currency) {
         Account account = new Account(accountId, IBAN, owner1, owner2, balance, currency);
-        accounts.add(account);
+        this.accounts.add(account);
+        owner1.getAccounts().add(account);
+        if (owner2 != null) owner2.getAccounts().add(account);
     }
 
     public Account findAccount(int accountId) {
