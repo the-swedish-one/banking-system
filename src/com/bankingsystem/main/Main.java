5e00cb036c6681274e795b4936b7f2f71534a5a8
S3M9F9 <alexandra_damgaard@swissre.com>
2024-09-27 19:30:32 +0200

    feat: Initial Main commit

---
+++ src/com/bankingsystem/main/Main.java
@@ -0,0 +1,10 @@
+package com.bankingsystem.main;
+
+import com.bankingsystem.models.*;
+import com.bankingsystem.services.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
