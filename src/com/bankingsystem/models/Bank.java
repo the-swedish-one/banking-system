141a6616aa119d6356d9141374fbccc67db8dc0e
S3M9F9 <alexandra_damgaard@swissre.com>
2024-10-04 00:38:39 +0200

    feat: add lists of users and accounts to bank variables

--- src/com/bankingsystem/models/Bank.java
+++ src/com/bankingsystem/models/Bank.java
@@ -1,23 +1,29 @@
 package com.bankingsystem.models;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Bank {
     private String bic;
-//    private List<Person> persons;
-//    private List<User> users;
-//    private List<Account> accounts;
+    private List<User> users;
+    private List<Account> accounts;
 
     public Bank(String bic) {
         this.bic = bic;
+        this.users = new ArrayList<>();
+        this.accounts = new ArrayList<>();
     }
 
     public String getBic() {
         return this.bic;
     }
 
-//    public List<Account> getAllAccounts() {
-//        return accounts;
-//    }
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
 
 }
