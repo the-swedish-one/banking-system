221ba509ec45e3f34edc7dc637464ab4106ae94b
S3M9F9 <alexandra_damgaard@swissre.com>
2024-10-04 11:49:07 +0200

    feat: initialise currency conversion rates

--- src/com/bankingsystem/persistence/dao/CurrencyConversionDAO.java
+++ src/com/bankingsystem/persistence/dao/CurrencyConversionDAO.java
@@ -1,19 +1,34 @@
 package com.bankingsystem.persistence.dao;
 
+import com.bankingsystem.models.CurrencyCode;
 import com.bankingsystem.models.CurrencyConversion;
 import com.bankingsystem.persistence.CurrencyConversionPersistenceService;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CurrencyConversionDAO implements CurrencyConversionPersistenceService {
 
-    private CurrencyConversion currentConversion;
+    private CurrencyConversion exchangeRates;
+
+    public CurrencyConversionDAO() {
+        Map<CurrencyCode, Double> initialRates = new HashMap<>();
+        initialRates.put(CurrencyCode.EUR, 1.0); // Base currency
+        initialRates.put(CurrencyCode.USD, 1.1);
+        initialRates.put(CurrencyCode.GBP, 0.9);
+        initialRates.put(CurrencyCode.SEK, 10.0);
+        initialRates.put(CurrencyCode.CHF, 1.2);
+
+        exchangeRates = new CurrencyConversion(initialRates);
+    }
 
     @Override
     public CurrencyConversion getLatestConversion() {
-        return currentConversion;
+        return exchangeRates;
     }
 
     @Override
     public void updateConversion(CurrencyConversion conversion) {
-        this.currentConversion = conversion;
+        this.exchangeRates = conversion;
     }
 }
