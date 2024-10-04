62f7ca9931181bf0eb60cc50f26b0f13c4ae04d6
S3M9F9 <alexandra_damgaard@swissre.com>
2024-10-04 12:35:02 +0200

    feat: add CRUD methods

--- src/com/bankingsystem/persistence/dao/BankDAO.java
+++ src/com/bankingsystem/persistence/dao/BankDAO.java
@@ -16,4 +16,35 @@ public class BankDAO implements BankPersistenceService {
         banks.add(bank);
     }
 
+    // Get bank by BIC
+    public Bank getBankByBic(String bic){
+        for (Bank bank : banks) {
+            if (bank.getBic().equals(bic)) {
+                return bank;
+            }
+        }
+        return null;
+    }
+
+    // Update bank
+    public void updateBank(Bank bank){
+        for (int i = 0; i < banks.size(); i++) {
+            if (banks.get(i).getBic().equals(bank.getBic())) {
+                banks.set(i, bank);
+                return;
+            }
+        }
+    }
+
+    //Delete bank by BIC
+    public boolean deleteBank(String bic){
+        for (int i = 0; i < banks.size(); i++) {
+            if (banks.get(i).getBic().equals(bic)) {
+                banks.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
