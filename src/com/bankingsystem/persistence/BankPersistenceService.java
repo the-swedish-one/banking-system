e98ba8d8a3e60334da152deeee058279b341ed7f
S3M9F9 <alexandra_damgaard@swissre.com>
2024-10-04 12:35:05 +0200

    feat: add CRUD methods

--- src/com/bankingsystem/persistence/BankPersistenceService.java
+++ src/com/bankingsystem/persistence/BankPersistenceService.java
@@ -5,4 +5,10 @@ import com.bankingsystem.models.Bank;
 public interface BankPersistenceService {
 
     void createBank(Bank bank);
+
+    Bank getBankByBic(String bic);
+
+    void updateBank(Bank bank);
+
+    boolean deleteBank(String bic);
 }
