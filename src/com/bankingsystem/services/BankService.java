aa2ed0566fdc41e5151e2b3c9e8c21dbfc66cecc
S3M9F9 <alexandra_damgaard@swissre.com>
2024-10-04 12:35:27 +0200

    feat: add getAllUsers and getAllAccounts for a bank

--- src/com/bankingsystem/services/BankService.java
+++ src/com/bankingsystem/services/BankService.java
@@ -1,8 +1,8 @@
 package com.bankingsystem.services;
 
+import com.bankingsystem.models.Account;
 import com.bankingsystem.models.Bank;
 import com.bankingsystem.persistence.BankPersistenceService;
-import com.bankingsystem.persistence.dao.BankDAO;
 
 public class BankService {
 
@@ -12,9 +12,37 @@ public class BankService {
         this.bankPersistenceService = bankPersistenceService;
     }
 
-    public Bank createBank(String bic) {
-        Bank bank = new Bank(bic);
+    public Bank createBank(String bankName, String bic) {
+        Bank bank = new Bank(bankName, bic);
         bankPersistenceService.createBank(bank);
         return bank;
+    }
+
+    // Get all users of the bank
+    public String getAllBankUsers(String bic) {
+        Bank bank = bankPersistenceService.getBankByBic(bic);
+        StringBuilder users = new StringBuilder();
+        users.append("Users of bank ").append(bank.getBankName()).append(":\n");
+        for (Account account : bank.getAccounts()) {
+            users.append(account.getOwner().toString()).append("\n");
+        }
+        if (users.length() == 0) {
+            return "No users found";
+        }
+        return users.toString();
+    }
+
+    // Get all accounts of the bank
+    public String getAllBankAccounts(String bic) {
+        Bank bank = bankPersistenceService.getBankByBic(bic);
+        StringBuilder accounts = new StringBuilder();
+        accounts.append("Accounts of bank ").append(bank.getBankName()).append(":\n");
+        for (Account account : bank.getAccounts()) {
+            accounts.append(account.toString()).append("\n");
+        }
+        if (accounts.length() == 0) {
+            return "No accounts found";
+        }
+        return accounts.toString();
     }
 }
