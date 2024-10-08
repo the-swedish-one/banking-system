9a99f59e6007672eacc0c9dcaecb78d89f2da231
S3M9F9 <alexandra_damgaard@swissre.com>
2024-10-08 20:37:20 +0200

    refactor: toString method

--- src/main/java/com/bankingsystem/models/User.java
+++ src/main/java/com/bankingsystem/models/User.java
@@ -34,8 +34,9 @@ public class User {
     @Override
     public String toString() {
         return "User{" +
-                "firstName=" + person.getFirstName() + '\'' +
-                ", lastName=" + person.getLastName() + '\'' +
+                "userId='" + userId + '\'' +
+                ", firstName='" + person.getFirstName() + '\'' +
+                ", lastName='" + person.getLastName() + '\'' +
                 '}';
     }
 
