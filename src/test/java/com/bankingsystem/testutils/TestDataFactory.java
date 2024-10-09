23f69730bc5aad988e8767977b30a06f7385613f
S3M9F9 <alexandra_damgaard@swissre.com>
2024-10-09 18:12:05 +0200

    feat: test data factory

---
+++ src/test/java/com/bankingsystem/testutils/TestDataFactory.java
@@ -0,0 +1,33 @@
+package com.bankingsystem.testutils;
+
+import com.bankingsystem.models.*;
+
+
+public class TestDataFactory {
+    public static Bank createBank(String name, String bic) {
+        return new Bank(name, bic);
+    }
+
+    public static Person createPerson(String firstName, String lastName, String email) {
+        return new Person(firstName, lastName, email, "Address Line 1", "Address Line 2", "City", "Country");
+    }
+
+    public static User createUser(String firstName, String lastName, String email) {
+        Person person = createPerson(firstName, lastName, email);
+        Bank bank = createBank("My Bank", "MB001");
+        return new User(person);
+    }
+
+    public static DepositTransaction createDepositTransaction(double amount) {
+        return new DepositTransaction(amount);
+    }
+
+    public static WithdrawTransaction createWithdrawTransaction(double amount) {
+        return new WithdrawTransaction(amount);
+    }
+
+    public static TransferTransaction createTransferTransaction(double amount, String fromAccountId, String toAccountId) {
+        return new TransferTransaction(amount, fromAccountId, toAccountId);
+    }
+
+}
