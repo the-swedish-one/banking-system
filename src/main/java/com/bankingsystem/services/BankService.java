210af8ca29cb6d5752f50ece8abb36a0c01253a9
S3M9F9 <alexandra_damgaard@swissre.com>
2024-10-10 09:55:54 +0200

    feat: add checks for null and empty results

--- src/main/java/com/bankingsystem/services/BankService.java
+++ src/main/java/com/bankingsystem/services/BankService.java
@@ -3,6 +3,9 @@ package com.bankingsystem.services;
 import com.bankingsystem.models.Account;
 import com.bankingsystem.models.Bank;
 import com.bankingsystem.models.User;
+import com.bankingsystem.models.exceptions.AccountNotFoundException;
+import com.bankingsystem.models.exceptions.BankNotFoundException;
+import com.bankingsystem.models.exceptions.UserNotFoundException;
 import com.bankingsystem.persistence.BankPersistenceService;
 
 public class BankService {
@@ -22,27 +25,37 @@ public class BankService {
     // Get all users of the bank
     public String getAllBankUsers(String bic) {
         Bank bank = bankPersistenceService.getBankByBic(bic);
+        if (bank == null) {
+            throw new BankNotFoundException("Bank not found");
+        }
+        if (bank.getUsers().isEmpty()) {
+            throw new UserNotFoundException("No users found");
+        }
+
         StringBuilder users = new StringBuilder();
         users.append("Users of bank ").append(bank.getBankName()).append(":\n");
         for (User user : bank.getUsers()) {
-            users.append(user.toString()).append("\n");
+            users.append(user.getPerson().getFirstName()).append(" ")
+                    .append(user.getPerson().getLastName()).append("\n");
         }
-        if (users.isEmpty()) {
-            return "No users found";
-        }
+
         return users.toString();
     }
+
 
     // Get all accounts of the bank
     public String getAllBankAccounts(String bic) {
         Bank bank = bankPersistenceService.getBankByBic(bic);
+        if (bank == null) {
+            throw new BankNotFoundException("Bank not found");
+        }
+        if (bank.getAccounts().isEmpty()) {
+            throw new AccountNotFoundException("No accounts found");
+        }
         StringBuilder accounts = new StringBuilder();
         accounts.append("Accounts of bank ").append(bank.getBankName()).append(":\n");
         for (Account account : bank.getAccounts()) {
             accounts.append(account.toString()).append("\n");
-        }
-        if (accounts.isEmpty()) {
-            return "No accounts found";
         }
         return accounts.toString();
     }
