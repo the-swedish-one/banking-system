f01f4f07971eb1f0e45212d2c5dfb3341a4c6699
S3M9F9 <alexandra_damgaard@swissre.com>
2024-11-05 15:26:04 +0100

    feat: add springboot application run

---
+++ banking-system/src/main/java/com/bankingsystem/Main.java
@@ -0,0 +1,11 @@
+package com.bankingsystem;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
