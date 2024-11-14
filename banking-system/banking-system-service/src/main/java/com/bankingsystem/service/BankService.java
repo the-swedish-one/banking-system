a95a95d4fb1df4ad7d22aa0d5411620bc3259c9b
S3M9F9 <alexandra_damgaard@swissre.com>
2024-11-14 12:16:54 +0100

    refactor: receive Bank from API layer and pass to persistence

--- banking-system/banking-system-service/src/main/java/com/bankingsystem/service/BankService.java
+++ banking-system/banking-system-service/src/main/java/com/bankingsystem/service/BankService.java
@@ -1,15 +1,11 @@
 package com.bankingsystem.service;
 
+import com.bankingsystem.exception.BankNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.bankingsystem.model.Account;
 import com.bankingsystem.model.Bank;
-import com.bankingsystem.exception.AccountNotFoundException;
-import com.bankingsystem.exception.BankNotFoundException;
-import com.bankingsystem.exception.UserNotFoundException;
 import com.bankingsystem.persistence.BankPersistenceService;
 
-import java.util.stream.Collectors;
 
 @Service
 public class BankService {
@@ -20,41 +16,30 @@ public class BankService {
         this.bankPersistenceService = bankPersistenceService;
     }
 
+    // Create and save a new bank
     public Bank createBank(String bankName, String bic) {
         Bank bank = new Bank(bankName, bic);
+        return bankPersistenceService.save(bank);
+    }
+
+    // Get bank by BIC
+    public Bank getBankByBic(String bic) {
+        Bank bank = bankPersistenceService.getBankByBic(bic);
+        if (bank == null) {
+            throw new BankNotFoundException("Bank not found");
+        }
+        return bank;
+    }
+
+    // Update bank
+    public Bank updateBank(String bic, String newBankName) {
+        Bank bank = bankPersistenceService.getBankByBic(bic);
+        if (bank == null) {
+            throw new BankNotFoundException("Bank not found");
+        }
+        bank.setBankName(newBankName);
         bankPersistenceService.save(bank);
         return bank;
     }
 
-    // Get all users of the bank
-    public String getAllBankUsers(String bic) {
-        Bank bank = bankPersistenceService.getBankByBic(bic);
-        if (bank == null) {
-            throw new BankNotFoundException("Bank not found");
-        }
-        if (bank.getUsers().isEmpty()) {
-            throw new UserNotFoundException("No users found");
-        }
-
-        return "Users of bank " + bank.getBankName() + ":\n" +
-                bank.getUsers().stream()
-                        .map(user -> user.getPerson().getFirstName() + " " + user.getPerson().getLastName())
-                        .collect(Collectors.joining("\n"));
-    }
-
-    // Get all accounts of the bank
-    public String getAllBankAccounts(String bic) {
-        Bank bank = bankPersistenceService.getBankByBic(bic);
-        if (bank == null) {
-            throw new BankNotFoundException("Bank not found");
-        }
-        if (bank.getAccounts().isEmpty()) {
-            throw new AccountNotFoundException("No accounts found");
-        }
-
-        return "Accounts of bank " + bank.getBankName() + ":\n" +
-                bank.getAccounts().stream()
-                        .map(Account::toString)
-                        .collect(Collectors.joining("\n"));
-    }
 }
