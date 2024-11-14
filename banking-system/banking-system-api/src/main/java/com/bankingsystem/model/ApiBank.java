a46e2e0a1fb35a5c7be54f2303eadcd3ae3993ee
S3M9F9 <alexandra_damgaard@swissre.com>
2024-11-14 12:14:54 +0100

    refactor: remove lists of users and accounts from bank

--- banking-system/banking-system-api/src/main/java/com/bankingsystem/model/ApiBank.java
+++ banking-system/banking-system-api/src/main/java/com/bankingsystem/model/ApiBank.java
@@ -4,9 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -15,8 +12,6 @@ public class ApiBank {
     private int id;
     private String bankName;
     private String bic;
-    private List<ApiUser> users = new ArrayList<>();
-    private List<ApiAccount> accounts = new ArrayList<>();
 
     public ApiBank(String bankName, String bic) {
         this.bankName = bankName;
