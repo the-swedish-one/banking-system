f8e85005d4419e335ce2f0ffc1b304cb56ab8f56
S3M9F9 <alexandra_damgaard@swissre.com>
2024-11-14 10:23:43 +0100

    feat: create ApiTransferTransactionMapper

---
+++ banking-system/banking-system-api/src/main/java/com/bankingsystem/mapper/ApiTransferTransactionMapper.java
@@ -0,0 +1,31 @@
+package com.bankingsystem.mapper;
+
+import com.bankingsystem.model.ApiTransferTransaction;
+import com.bankingsystem.model.TransferTransaction;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApiTransferTransactionMapper {
+
+    public TransferTransaction toServiceModel(ApiTransferTransaction apiModel) {
+        if (apiModel == null) return null;
+        TransferTransaction model = new TransferTransaction();
+        model.setTransactionId(apiModel.getTransactionId());
+        model.setAmount(apiModel.getAmount());
+        model.setTimestamp(apiModel.getTimestamp());
+        model.setToAccountId(apiModel.getToAccountId());
+        model.setFromAccountId(apiModel.getFromAccountId());
+        return model;
+    }
+
+    public ApiTransferTransaction toApiModel(TransferTransaction model) {
+        if (model == null) return null;
+        ApiTransferTransaction apiModel = new ApiTransferTransaction();
+        apiModel.setTransactionId(model.getTransactionId());
+        apiModel.setAmount(model.getAmount());
+        apiModel.setTimestamp(model.getTimestamp());
+        apiModel.setToAccountId(model.getToAccountId());
+        apiModel.setFromAccountId(model.getFromAccountId());
+        return apiModel;
+    }
+}
