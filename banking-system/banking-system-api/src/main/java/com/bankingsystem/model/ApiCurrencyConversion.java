f264e6c7669e719ad3e7dc0de5699887fe2ebc16
S3M9F9 <alexandra_damgaard@swissre.com>
2024-11-14 10:21:40 +0100

    feat: create ApiCurrencyConversion

---
+++ banking-system/banking-system-api/src/main/java/com/bankingsystem/model/ApiCurrencyConversion.java
@@ -0,0 +1,23 @@
+package com.bankingsystem.model;
+
+import com.bankingsystem.enums.CurrencyCode;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.util.Date;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiCurrencyConversion {
+
+    private int id;
+    private Date lastUpdated;
+    private Map<CurrencyCode, Double> exchangeRates;
+
+    public ApiCurrencyConversion(Map<CurrencyCode, Double> exchangeRates) {
+        this.exchangeRates = exchangeRates;
+        this.lastUpdated = new Date();
+    }
+}
