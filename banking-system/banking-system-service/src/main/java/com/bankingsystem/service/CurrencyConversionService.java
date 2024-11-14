22a4d83bca472745e08b154c32450d1327c79a1a
S3M9F9 <alexandra_damgaard@swissre.com>
2024-11-14 11:01:52 +0100

    feat: refactor to user mappers and use new layered architecture

--- banking-system/banking-system-service/src/main/java/com/bankingsystem/service/CurrencyConversionService.java
+++ banking-system/banking-system-service/src/main/java/com/bankingsystem/service/CurrencyConversionService.java
@@ -1,6 +1,7 @@
 package com.bankingsystem.service;
 
 import com.bankingsystem.enums.CurrencyCode;
+import com.bankingsystem.mapper.CurrencyConversionMapper;
 import com.bankingsystem.model.CurrencyConversion;
 import com.bankingsystem.persistence.CurrencyConversionPersistenceService;
 import org.springframework.stereotype.Service;
@@ -14,22 +15,24 @@ import java.util.Map;
 public class CurrencyConversionService {
 
     private final CurrencyConversionPersistenceService currencyConversionPersistenceService;
+    private final CurrencyConversionMapper currencyConversionMapper;
 
-    public CurrencyConversionService(CurrencyConversionPersistenceService currencyConversionPersistenceService) {
+    public CurrencyConversionService(CurrencyConversionPersistenceService currencyConversionPersistenceService,
+                                     CurrencyConversionMapper currencyConversionMapper) {
         this.currencyConversionPersistenceService = currencyConversionPersistenceService;
+        this.currencyConversionMapper = currencyConversionMapper;
     }
 
     // Convert an amount from one currency to another
     public BigDecimal convertAmount(BigDecimal amount, CurrencyCode fromCurrency, CurrencyCode toCurrency) {
-        CurrencyConversion conversion = currencyConversionPersistenceService.getLatestConversion();
-        BigDecimal rate = BigDecimal.valueOf(getExchangeRate(fromCurrency, toCurrency));
+        CurrencyConversion conversion = getLatestCurrencyConversion();
+        BigDecimal rate = BigDecimal.valueOf(getExchangeRate(conversion, fromCurrency, toCurrency));
         BigDecimal convertedAmount = amount.multiply(rate);
         return convertedAmount.setScale(2, RoundingMode.HALF_UP);
     }
 
     // Get rate for exchanging one currency to another
-    public double getExchangeRate(CurrencyCode fromCurrency, CurrencyCode toCurrency) {
-        CurrencyConversion conversion = currencyConversionPersistenceService.getLatestConversion();
+    public double getExchangeRate(CurrencyConversion conversion, CurrencyCode fromCurrency, CurrencyCode toCurrency) {
         if (fromCurrency == toCurrency) {
             return 1.0;
         }
@@ -42,10 +45,18 @@ public class CurrencyConversionService {
         return toRate / fromRate;
     }
 
+    // Retrieve the latest currency conversion data
+    private CurrencyConversion getLatestCurrencyConversion() {
+        var conversionEntity = currencyConversionPersistenceService.getLatestConversion();
+        return currencyConversionMapper.toModel(conversionEntity);
+    }
+
+    // Update all exchange rates
     // Update all exchange rates
     public void updateExchangeRates(Map<CurrencyCode, Double> newRates) {
         CurrencyConversion conversion = new CurrencyConversion(newRates);
         conversion.setLastUpdated(new Date());
-        currencyConversionPersistenceService.updateConversion(conversion);
+        var conversionEntity = currencyConversionMapper.toEntity(conversion);
+        currencyConversionPersistenceService.updateConversion(conversionEntity);
     }
 }
