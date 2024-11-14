217072108f607b498b1b408af029eb417b266f9b
S3M9F9 <alexandra_damgaard@swissre.com>
2024-11-14 12:56:58 +0100

    fix: create instance of PersonDetailsMapper

---
+++ banking-system/banking-system-service/src/main/java/com/bankingsystem/mapper/UserMapper.java
@@ -0,0 +1,31 @@
+package com.bankingsystem.mapper;
+
+import com.bankingsystem.model.UserEntity;
+import com.bankingsystem.model.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+    private final PersonDetailsMapper personDetailsMapper;
+
+    public UserMapper(PersonDetailsMapper personDetailsMapper) {
+        this.personDetailsMapper = personDetailsMapper;
+    }
+
+    public User toModel(UserEntity entity) {
+        if (entity == null) return null;
+        User user = new User();
+        user.setUserId(entity.getUserId());
+        user.setPerson(personDetailsMapper.toModel(entity.getPerson()));
+        return user;
+    }
+
+    public UserEntity toEntity(User model) {
+        if (model == null) return null;
+        UserEntity entity = new UserEntity();
+        entity.setUserId(model.getUserId());
+        entity.setPerson(personDetailsMapper.toEntity(model.getPerson()));
+        return entity;
+    }
+}
