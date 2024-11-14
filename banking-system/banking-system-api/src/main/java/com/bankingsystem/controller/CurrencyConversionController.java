4f74ac9adc255b01813ef61f467c4508e6d073db
S3M9F9 <alexandra_damgaard@swissre.com>
2024-11-14 11:00:50 +0100

    feat: create CurrencyConversionController

--- banking-system/banking-system-api/src/main/java/com/bankingsystem/controller/CurrencyConversionController.java
+++ banking-system/banking-system-api/src/main/java/com/bankingsystem/controller/CurrencyConversionController.java
@@ -1,7 +1,46 @@
 package com.bankingsystem.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.bankingsystem.enums.CurrencyCode;
+import com.bankingsystem.mapper.ApiCurrencyConversionMapper;
+import com.bankingsystem.model.ApiCurrencyConversion;
+import com.bankingsystem.service.CurrencyConversionService;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/api/currency-conversion")
 public class CurrencyConversionController {
+
+    private final CurrencyConversionService currencyConversionService;
+    private final ApiCurrencyConversionMapper apiCurrencyConversionMapper;
+
+    public CurrencyConversionController(CurrencyConversionService currencyConversionService, ApiCurrencyConversionMapper apiCurrencyConversionMapper) {
+        this.currencyConversionService = currencyConversionService;
+        this.apiCurrencyConversionMapper = apiCurrencyConversionMapper;
+    }
+
+    @PostMapping("/convert")
+    public BigDecimal convertAmount(@RequestParam BigDecimal amount,
+                                    @RequestParam CurrencyCode fromCurrency,
+                                    @RequestParam CurrencyCode toCurrency) {
+        return currencyConversionService.convertAmount(amount, fromCurrency, toCurrency);
+    }
+
+    @GetMapping("/rate")
+    public double getExchangeRate(@RequestParam CurrencyCode fromCurrency,
+                                  @RequestParam CurrencyCode toCurrency) {
+        return currencyConversionService.getExchangeRate(fromCurrency, toCurrency);
+    }
+
+    @PutMapping("/update-rates")
+    public ApiCurrencyConversion updateExchangeRates(@RequestBody ApiCurrencyConversion apiCurrencyConversion) {
+        Map<CurrencyCode, Double> newRates = apiCurrencyConversion.getExchangeRates();
+        currencyConversionService.updateExchangeRates(newRates);
+
+        // Return updated exchange rates as response
+        return apiCurrencyConversionMapper.toApiModel(
+                currencyConversionService.getLatestConversion());
+    }
 }
