65efbffa22441b3be5107f6d30e0eab764398365
S3M9F9 <alexandra_damgaard@swissre.com>
2024-11-14 10:27:20 +0100

    feat: create CurrencyConversionMapper

---
+++ banking-system/banking-system-service/src/main/java/com/bankingsystem/mapper/CurrencyConversionMapper.java
@@ -0,0 +1,27 @@
+package com.bankingsystem.mapper;
+
+import com.bankingsystem.model.CurrencyConversion;
+import com.bankingsystem.model.CurrencyConversionEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CurrencyConversionMapper {
+
+    public CurrencyConversion toModel(CurrencyConversionEntity entity) {
+        if (entity == null) return null;
+        CurrencyConversion currencyConversion = new CurrencyConversion();
+        currencyConversion.setId(entity.getId());
+        currencyConversion.setLastUpdated(entity.getLastUpdated());
+        currencyConversion.setExchangeRates(entity.getExchangeRates());
+        return currencyConversion;
+    }
+
+    public CurrencyConversionEntity toEntity(CurrencyConversion model) {
+        if (model == null) return null;
+        CurrencyConversionEntity entity = new CurrencyConversionEntity();
+        entity.setId(model.getId());
+        entity.setLastUpdated(model.getLastUpdated());
+        entity.setExchangeRates(model.getExchangeRates());
+        return entity;
+    }
+}
