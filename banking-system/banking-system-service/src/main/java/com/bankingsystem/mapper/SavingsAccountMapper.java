98af31d44f6233db55a767ab5ff204e1ea01923c
S3M9F9 <alexandra_damgaard@swissre.com>
2024-11-14 13:03:53 +0100

    fix: create instance of userMapper

---
+++ banking-system/banking-system-service/src/main/java/com/bankingsystem/mapper/SavingsAccountMapper.java
@@ -0,0 +1,41 @@
+package com.bankingsystem.mapper;
+
+import com.bankingsystem.model.SavingsAccount;
+import com.bankingsystem.model.SavingsAccountEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SavingsAccountMapper {
+
+    private final UserMapper userMapper;
+
+    public SavingsAccountMapper(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+
+    public SavingsAccount toModel (SavingsAccountEntity entity) {
+        if (entity == null) {return null;}
+        SavingsAccount model = new SavingsAccount();
+        model.setAccountId(entity.getAccountId());
+        model.setIban(entity.getIban());
+        model.setAccountName(entity.getAccountName());
+        model.setOwner(userMapper.toModel(entity.getOwner()));
+        model.setBalance(entity.getBalance());
+        model.setCurrency(entity.getCurrency());
+        model.setInterestRatePercentage(entity.getInterestRatePercentage());
+        return model;
+    }
+
+    public SavingsAccountEntity toEntity (SavingsAccount model) {
+        if (model == null) {return null;}
+        SavingsAccountEntity entity = new SavingsAccountEntity();
+        entity.setAccountId(model.getAccountId());
+        entity.setIban(model.getIban());
+        entity.setAccountName(model.getAccountName());
+        entity.setOwner(userMapper.toEntity(model.getOwner()));
+        entity.setBalance(model.getBalance());
+        entity.setCurrency(model.getCurrency());
+        entity.setInterestRatePercentage(model.getInterestRatePercentage());
+        return entity;
+    }
+}
