8873c8b1c17375b738d8248177ced275bc3f6ae7
S3M9F9 <alexandra_damgaard@swissre.com>
2024-11-14 13:04:28 +0100

    fix: create ApiUserMapper

---
+++ banking-system/banking-system-api/src/main/java/com/bankingsystem/mapper/ApiUserMapper.java
@@ -0,0 +1,33 @@
+package com.bankingsystem.mapper;
+
+import com.bankingsystem.model.ApiUser;
+import com.bankingsystem.model.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApiUserMapper {
+
+    private final ApiPersonDetailsMapper apiPersonDetailsMapper;
+
+    public ApiUserMapper(ApiPersonDetailsMapper apiPersonDetailsMapper) {
+        this.apiPersonDetailsMapper = apiPersonDetailsMapper;
+    }
+
+//    Mapstruct??
+
+    public ApiUser toApiModel(User user) {
+        if (user == null) return null;
+        ApiUser apiUser = new ApiUser();
+        apiUser.setUserId(user.getUserId());
+        apiUser.setPerson(apiPersonDetailsMapper.toApiModel(user.getPerson()));
+        return apiUser;
+    }
+
+    public User toServiceModel(ApiUser apiUser) {
+        if (apiUser == null) return null;
+        User user = new User();
+        user.setUserId(apiUser.getUserId());
+        user.setPerson(apiPersonDetailsMapper.toServiceModel(apiUser.getPerson()));
+        return user;
+    }
+}
