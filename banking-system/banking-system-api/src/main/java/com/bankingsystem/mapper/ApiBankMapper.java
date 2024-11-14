5c7bcdbfaef7b08c4adac573d04446e2ab392427
S3M9F9 <alexandra_damgaard@swissre.com>
2024-11-14 12:15:18 +0100

    refactor: remove lists of users and accounts from bank

---
+++ banking-system/banking-system-api/src/main/java/com/bankingsystem/mapper/ApiBankMapper.java
@@ -0,0 +1,25 @@
+package com.bankingsystem.mapper;
+
+import com.bankingsystem.model.ApiBank;
+import com.bankingsystem.model.Bank;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApiBankMapper {
+
+    public Bank toServiceModel(ApiBank apiModel) {
+        if (apiModel == null) return null;
+        Bank bank = new Bank();
+        bank.setBankName(apiModel.getBankName());
+        bank.setBic(apiModel.getBic());
+        return bank;
+    }
+
+    public ApiBank toApiModel(Bank serviceModel) {
+        if (serviceModel == null) return null;
+        ApiBank apiBank = new ApiBank();
+        apiBank.setBankName(serviceModel.getBankName());
+        apiBank.setBic(serviceModel.getBic());
+        return apiBank;
+    }
+}
