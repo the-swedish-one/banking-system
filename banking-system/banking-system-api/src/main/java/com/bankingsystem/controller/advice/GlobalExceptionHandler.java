8d9dc0d009d574b6034d73970866033fdfa66645
S3M9F9 <alexandra_damgaard@swissre.com>
2024-11-21 17:23:28 +0100

    feat: add logging

--- banking-system/banking-system-api/src/main/java/com/bankingsystem/controller/advice/GlobalExceptionHandler.java
+++ banking-system/banking-system-api/src/main/java/com/bankingsystem/controller/advice/GlobalExceptionHandler.java
@@ -3,65 +3,122 @@ package com.bankingsystem.controller.advice;
 import com.bankingsystem.exception.*;
 import com.bankingsystem.model.ErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+//    Project specific exceptions
     @ExceptionHandler(InsufficientFundsException.class)
     public ResponseEntity<ErrorResponse> handleInsufficientFundsException(InsufficientFundsException ex, HttpServletRequest request) {
-        ErrorResponse errorResponse = new ErrorResponse(String.valueOf(HttpStatus.BAD_REQUEST.value()), "Insufficient funds: " + ex.getMessage(), request.getRequestURI());
+        logger.error("Insufficient Funds Exception: {}", ex.getMessage(), ex);
+        ErrorResponse errorResponse = new ErrorResponse(
+                String.valueOf(HttpStatus.BAD_REQUEST.value()),
+                "Insufficient funds: " + ex.getMessage(),
+                request.getRequestURI());
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(OverdraftLimitExceededException.class)
     public ResponseEntity<ErrorResponse> handleOverdraftLimitExceededException(OverdraftLimitExceededException ex, HttpServletRequest request) {
-        ErrorResponse errorResponse = new ErrorResponse(String.valueOf(HttpStatus.BAD_REQUEST.value()), "Overdraft limit exceeded: " + ex.getMessage(), request.getRequestURI());
+        logger.error("Overdraft Limit Exceeded Exception: {}", ex.getMessage(), ex);
+        ErrorResponse errorResponse = new ErrorResponse(
+                String.valueOf(HttpStatus.BAD_REQUEST.value()),
+                "Overdraft limit exceeded: " + ex.getMessage(),
+                request.getRequestURI());
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(AccountNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleAccountNotFoundException(AccountNotFoundException ex, HttpServletRequest request) {
-        ErrorResponse errorResponse = new ErrorResponse(String.valueOf(HttpStatus.NOT_FOUND.value()), "Account not found: " + ex.getMessage(), request.getRequestURI());
+        logger.error("Account Not Found Exception: {}", ex.getMessage(), ex);
+        ErrorResponse errorResponse = new ErrorResponse(
+                String.valueOf(HttpStatus.NOT_FOUND.value()),
+                "Account not found: " + ex.getMessage(),
+                request.getRequestURI());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(PersonDetailsNotFoundException.class)
     public ResponseEntity<ErrorResponse> handlePersonDetailsNotFoundException(PersonDetailsNotFoundException ex, HttpServletRequest request) {
-        ErrorResponse errorResponse = new ErrorResponse(String.valueOf(HttpStatus.NOT_FOUND.value()), "Person Details not found: " + ex.getMessage(), request.getRequestURI());
+        logger.error("Person Details Not Found Exception: {}", ex.getMessage(), ex);
+        ErrorResponse errorResponse = new ErrorResponse(
+                String.valueOf(HttpStatus.NOT_FOUND.value()),
+                "Person Details not found: " + ex.getMessage(),
+                request.getRequestURI());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(TransactionNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleTransactionNotFoundException(TransactionNotFoundException ex, HttpServletRequest request) {
-        ErrorResponse errorResponse = new ErrorResponse(String.valueOf(HttpStatus.NOT_FOUND.value()), "Transaction not found: " + ex.getMessage(), request.getRequestURI());
+        logger.error("Transaction Not Found Exception: {}", ex.getMessage(), ex);
+        ErrorResponse errorResponse = new ErrorResponse(
+                String.valueOf(HttpStatus.NOT_FOUND.value()),
+                "Transaction not found: " + ex.getMessage(),
+                request.getRequestURI());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException ex, HttpServletRequest request) {
-        ErrorResponse errorResponse = new ErrorResponse(String.valueOf(HttpStatus.NOT_FOUND.value()), "User not found: " + ex.getMessage(), request.getRequestURI());
+        logger.error("User Not Found Exception: {}", ex.getMessage(), ex);
+        ErrorResponse errorResponse = new ErrorResponse(
+                String.valueOf(HttpStatus.NOT_FOUND.value()),
+                "User not found: " + ex.getMessage(),
+                request.getRequestURI());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(BankNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleBankNotFoundException(BankNotFoundException ex, HttpServletRequest request) {
-        ErrorResponse errorResponse = new ErrorResponse(String.valueOf(HttpStatus.NOT_FOUND.value()), "Bank not found: " + ex.getMessage(), request.getRequestURI());
+        logger.error("Bank Not Found Exception: {}", ex.getMessage(), ex);
+        ErrorResponse errorResponse = new ErrorResponse(
+                String.valueOf(HttpStatus.NOT_FOUND.value()),
+                "Bank not found: " + ex.getMessage(),
+                request.getRequestURI());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
+//    Generic exceptions
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex, HttpServletRequest request) {
-        ErrorResponse errorResponse = new ErrorResponse(String.valueOf(HttpStatus.BAD_REQUEST.value()), "Invalid input: " + ex.getMessage(), request.getRequestURI());
+        logger.error("Illegal Argument Exception: {}", ex.getMessage(), ex);
+        ErrorResponse errorResponse = new ErrorResponse(
+                String.valueOf(HttpStatus.BAD_REQUEST.value()),
+                "Invalid input: " + ex.getMessage(),
+                request.getRequestURI())
+                ;
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(org.springframework.http.converter.HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(org.springframework.http.converter.HttpMessageNotReadableException ex, WebRequest request) {
+        logger.error("HTTP Message Not Readable Exception: {}", ex.getMessage(), ex);
+        ErrorResponse errorResponse = new ErrorResponse(
+                "INVALID_JSON",
+                "Request body is malformed",
+                request.getDescription(false).replace("uri=", "")
+        );
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGenericException(Exception ex, HttpServletRequest request) {
-        ErrorResponse errorResponse = new ErrorResponse(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()), "An error occurred: " + ex.getMessage(), request.getRequestURI());
+        logger.error("Generic Exception: {}", ex.getMessage(), ex);
+        ErrorResponse errorResponse = new ErrorResponse(
+                String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()),
+                "An error occurred: " + ex.getMessage(),
+                request.getRequestURI()
+        );
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
 }
