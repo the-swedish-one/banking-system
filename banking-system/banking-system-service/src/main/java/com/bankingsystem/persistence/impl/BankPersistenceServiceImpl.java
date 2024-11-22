aed8837f22e6d991c71eae6f9eabb142b6a1a673
S3M9F9 <alexandra_damgaard@swissre.com>
2024-11-22 17:10:37 +0100

    feat: add logging

--- banking-system/banking-system-service/src/main/java/com/bankingsystem/persistence/impl/BankPersistenceServiceImpl.java
+++ banking-system/banking-system-service/src/main/java/com/bankingsystem/persistence/impl/BankPersistenceServiceImpl.java
@@ -6,6 +6,8 @@ import com.bankingsystem.model.Bank;
 import com.bankingsystem.model.BankEntity;
 import com.bankingsystem.persistence.BankPersistenceService;
 import com.bankingsystem.repository.BankRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,8 @@ import java.util.Optional;
 
 @Service
 public class BankPersistenceServiceImpl implements BankPersistenceService {
+
+    private static final Logger logger = LoggerFactory.getLogger(BankPersistenceServiceImpl.class);
 
     private final BankRepository bankRepository;
     private final BankMapper bankMapper;
@@ -26,23 +30,21 @@ public class BankPersistenceServiceImpl implements BankPersistenceService {
     // Create or update bank
     @Override
     public Bank save(Bank bank){
+        logger.info("Saving new bank");
         BankEntity entity = bankMapper.toEntity(bank);
         BankEntity savedEntity = bankRepository.save(entity);
+        logger.info("Successfully saved bank with BIC: {}", savedEntity.getBic());
         return bankMapper.toModel(savedEntity);
     }
 
     // Get bank by BIC
     @Override
     public Bank getBankByBic(String bic) {
-        BankEntity entity = bankRepository.findByBic(bic).orElseThrow(() -> new BankNotFoundException("Bank not found"));
+        BankEntity entity = bankRepository.findByBic(bic).orElseThrow(() -> {
+            logger.error("Bank not found");
+            return new BankNotFoundException("Bank not found");
+        });
+        logger.info("Successfully fetched bank with BIC: {}", bic);
         return bankMapper.toModel(entity);
-    }
-
-    // Delete bank by BIC
-    @Override
-    public boolean deleteBank(String bic) {
-        Optional<BankEntity> bankToDelete = bankRepository.findByBic(bic);
-        bankToDelete.ifPresent(bankRepository::delete);
-        return bankToDelete.isPresent();
     }
 }
--- banking-system/banking-system-service/src/main/java/com/bankingsystem/service/BankService.java
+++ banking-system/banking-system-service/src/main/java/com/bankingsystem/service/BankService.java
@@ -1,6 +1,8 @@
 package com.bankingsystem.service;
 
 import com.bankingsystem.exception.BankNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.bankingsystem.model.Bank;
@@ -10,6 +12,8 @@ import com.bankingsystem.persistence.BankPersistenceService;
 @Service
 public class BankService {
 
+    private static final Logger logger = LoggerFactory.getLogger(JointCheckingAccountService.class);
+
     private final BankPersistenceService bankPersistenceService;
 
     public BankService(BankPersistenceService bankPersistenceService) {
@@ -18,14 +22,18 @@ public class BankService {
 
     // Create and save a new bank
     public Bank createBank(String bankName, String bic) {
+        logger.info("Creating new bank");
         Bank bank = new Bank(bankName, bic);
+        logger.info("Successfully created new bank with BIC: {}", bic);
         return bankPersistenceService.save(bank);
     }
 
     // Get bank by BIC
     public Bank getBankByBic(String bic) {
+        logger.info("Fetching bank by BIC: {}", bic);
         Bank bank = bankPersistenceService.getBankByBic(bic);
         if (bank == null) {
+            logger.error("Bank not found");
             throw new BankNotFoundException("Bank not found");
         }
         return bank;
@@ -33,10 +41,8 @@ public class BankService {
 
     // Update bank
     public Bank updateBank(String bic, String newBankName) {
+        logger.info("Updating bank with BIC: {}", bic);
         Bank bank = bankPersistenceService.getBankByBic(bic);
-        if (bank == null) {
-            throw new BankNotFoundException("Bank not found");
-        }
         bank.setBankName(newBankName);
         bankPersistenceService.save(bank);
         return bank;
