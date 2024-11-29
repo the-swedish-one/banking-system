4b247a050246bcca199735a54cc112410a3571ad
S3M9F9 <alexandra_damgaard@swissre.com>
2024-11-29 10:57:17 +0100

    feat: add Lombok annotations

--- banking-system/banking-system-persistence/src/main/java/com/bankingsystem/model/BankEntity.java
+++ banking-system/banking-system-persistence/src/main/java/com/bankingsystem/model/BankEntity.java
@@ -1,12 +1,17 @@
 package com.bankingsystem.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Table(name = "bank")
 @Entity
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BankEntity {
 
     @Id
