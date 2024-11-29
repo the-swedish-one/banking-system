6c45b87f8a2b4acf3197dcd718b34738b4ba45ab
S3M9F9 <alexandra_damgaard@swissre.com>
2024-11-29 10:56:38 +0100

    feat: add constructor for tests with ID

--- banking-system/banking-system-service/src/main/java/com/domain/model/CheckingAccount.java
+++ banking-system/banking-system-service/src/main/java/com/domain/model/CheckingAccount.java
@@ -28,24 +28,32 @@ public class CheckingAccount implements Withdrawable, Depositable  {
         this.overdraftLimit = overdraftLimit;
     }
 
+    public CheckingAccount(int Id, User owner, BigDecimal balance, CurrencyCode currency, BigDecimal overdraftLimit) {
+        this.accountId = Id;
+        this.owner = owner;
+        this.balance = balance;
+        this.currency = currency;
+        this.overdraftLimit = overdraftLimit;
+    }
+
     @Override
     public void withdraw(BigDecimal amount) {
         if (amount.compareTo(BigDecimal.ZERO) <= 0) {
-            throw new IllegalArgumentException("Withdraw Failed: Amount must be greater than 0");
+            throw new IllegalArgumentException("Amount must be greater than 0");
         }
 
         BigDecimal availableBalance = this.balance.add(this.overdraftLimit);
         if (availableBalance.compareTo(amount) >= 0) {
             this.balance = this.balance.subtract(amount);
         } else {
-            throw new OverdraftLimitExceededException("Withdraw Failed: Overdraft limit exceeded");
+            throw new OverdraftLimitExceededException("Overdraft limit exceeded");
         }
     }
 
     @Override
     public void deposit(BigDecimal amount) {
         if (amount.compareTo(BigDecimal.ZERO) <= 0) {
-            throw new IllegalArgumentException("Deposit failed: Amount must be greater than 0");
+            throw new IllegalArgumentException("Amount must be greater than 0");
         }
         this.balance = this.balance.add(amount);
     }
--- banking-system/banking-system-service/src/main/java/com/domain/model/JointCheckingAccount.java
+++ banking-system/banking-system-service/src/main/java/com/domain/model/JointCheckingAccount.java
@@ -25,27 +25,36 @@ public class JointCheckingAccount implements Withdrawable, Depositable {
     public JointCheckingAccount(User owner, User secondOwner, BigDecimal balance, CurrencyCode currency, BigDecimal overdraftLimit) {
         this.owner = owner;
         this.balance = balance;
+        this.currency = currency;
+        this.secondOwner = secondOwner;
+    }
+
+    public JointCheckingAccount(int Id, User owner, User secondOwner, BigDecimal balance, CurrencyCode currency, BigDecimal overdraftLimit) {
+        this.accountId = Id;
+        this.owner = owner;
+        this.balance = balance;
+        this.currency = currency;
         this.secondOwner = secondOwner;
     }
 
     @Override
     public void withdraw(BigDecimal amount) {
         if (amount.compareTo(BigDecimal.ZERO) <= 0) {
-            throw new IllegalArgumentException("Withdraw Failed: Amount must be greater than 0");
+            throw new IllegalArgumentException("Amount must be greater than 0");
         }
 
         BigDecimal availableBalance = this.balance.add(this.overdraftLimit);
         if (availableBalance.compareTo(amount) >= 0) {
             this.balance = this.balance.subtract(amount);
         } else {
-            throw new OverdraftLimitExceededException("Withdraw Failed: Overdraft limit exceeded");
+            throw new OverdraftLimitExceededException("Overdraft limit exceeded");
         }
     }
 
     @Override
     public void deposit(BigDecimal amount) {
         if (amount.compareTo(BigDecimal.ZERO) <= 0) {
-            throw new IllegalArgumentException("Deposit failed: Amount must be greater than 0");
+            throw new IllegalArgumentException("Amount must be greater than 0");
         }
         this.balance = this.balance.add(amount);
     }
