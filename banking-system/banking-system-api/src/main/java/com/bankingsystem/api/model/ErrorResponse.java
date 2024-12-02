495a7addd2bba5057b09459651279595f8bccd6e
S3M9F9 <alexandra_damgaard@swissre.com>
2024-12-02 15:59:33 +0100

    feat: change Instant to ZonedDateTime

--- banking-system/banking-system-api/src/main/java/com/bankingsystem/api/model/ErrorResponse.java
+++ banking-system/banking-system-api/src/main/java/com/bankingsystem/api/model/ErrorResponse.java
@@ -3,23 +3,23 @@ package com.bankingsystem.api.model;
 import lombok.*;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import java.time.Instant;
+import java.time.ZonedDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ErrorResponse {
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
-    private Instant timestamp = Instant.now();
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX", timezone = "UTC")
+    private ZonedDateTime timestamp = ZonedDateTime.now();
 
     private String errorCode;
     private String message;
     private String path;
 
     public ErrorResponse(String errorCode, String message, String path) {
-        this.timestamp = Instant.now();
-        this.errorCode = String.valueOf(errorCode);
+        this.timestamp = ZonedDateTime.now();
+        this.errorCode = errorCode;
         this.message = message;
         this.path = path;
     }
