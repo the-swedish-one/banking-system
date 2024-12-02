de234fe054f066179a7366d060836c475626354f
S3M9F9 <alexandra_damgaard@swissre.com>
2024-12-02 12:31:36 +0100

    feat: add scanBasePackages

--- banking-system/banking-system-api/src/main/java/com/bankingsystem/api/Main.java
+++ banking-system/banking-system-api/src/main/java/com/bankingsystem/api/Main.java
@@ -1,9 +1,9 @@
-package com.bankingsystem;
+package com.bankingsystem.api;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.bankingsystem.api", "com.bankingsystem.domain", "com.bankingsystem.persistence"})
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
