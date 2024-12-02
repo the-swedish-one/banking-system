24b117f58672d042457ae5eb0bf823e76b9881e3
S3M9F9 <alexandra_damgaard@swissre.com>
2024-12-02 15:38:27 +0100

    feat: add EntityScan and EnableJpaRepositories

--- banking-system/banking-system-api/src/main/java/com/bankingsystem/api/Main.java
+++ banking-system/banking-system-api/src/main/java/com/bankingsystem/api/Main.java
@@ -2,8 +2,12 @@ package com.bankingsystem.api;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.bankingsystem.api", "com.bankingsystem.domain", "com.bankingsystem.persistence"})
+@EntityScan(basePackages = "com.bankingsystem.persistence.model")
+@EnableJpaRepositories(basePackages = "com.bankingsystem.persistence.repository")
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
