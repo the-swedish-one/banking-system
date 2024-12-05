9432958554b592e4366025b75ca37e874a0f5d10
S3M9F9 <alexandra_damgaard@swissre.com>
2024-12-05 14:01:34 +0100

    refactor: rename route to singular

--- banking-system/banking-system-api/src/main/java/com/bankingsystem/api/controller/SavingsAccountController.java
+++ banking-system/banking-system-api/src/main/java/com/bankingsystem/api/controller/SavingsAccountController.java
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/savings-accounts")
+@RequestMapping("/api/savings-account")
 public class SavingsAccountController {
 
     private final SavingsAccountService savingsAccountService;
