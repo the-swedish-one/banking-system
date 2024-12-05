051a8c7486e314e474f6adcfa25eb2e4747eeed4
S3M9F9 <alexandra_damgaard@swissre.com>
2024-12-05 12:58:13 +0100

    feat: add mapping

--- banking-system/banking-system-api/src/main/java/com/bankingsystem/api/mapper/ApiPersonDetailsMapper.java
+++ banking-system/banking-system-api/src/main/java/com/bankingsystem/api/mapper/ApiPersonDetailsMapper.java
@@ -3,14 +3,13 @@ package com.bankingsystem.api.mapper;
 import com.bankingsystem.api.model.ApiPersonDetails;
 import com.bankingsystem.domain.model.PersonDetails;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ApiPersonDetailsMapper {
-
-    // Map from service model PersonDetails to API model ApiPersonDetails
     ApiPersonDetails toApiModel(PersonDetails personDetails);
 
-    // Map from API model ApiPersonDetails to service model PersonDetails
+    @Mapping(target = "personId", source = "personId")
     PersonDetails toServiceModel(ApiPersonDetails apiPersonDetails);
 }
 
