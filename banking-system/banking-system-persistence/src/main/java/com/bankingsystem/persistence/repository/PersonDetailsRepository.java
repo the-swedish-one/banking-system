e700a9381910e1dac8a3a932e8b895b1a47b6d0e
S3M9F9 <alexandra_damgaard@swissre.com>
2024-12-05 13:00:18 +0100

    feat: add existsByEmail

--- banking-system/banking-system-persistence/src/main/java/com/bankingsystem/persistence/repository/PersonDetailsRepository.java
+++ banking-system/banking-system-persistence/src/main/java/com/bankingsystem/persistence/repository/PersonDetailsRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PersonDetailsRepository extends JpaRepository <PersonDetailsEntity, Integer> {
+    boolean existsByEmail(String email);
 }
