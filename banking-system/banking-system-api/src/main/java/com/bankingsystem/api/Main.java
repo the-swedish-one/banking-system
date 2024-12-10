e51dd115f5f087b94e0dabd848fb0add3fbbee4e
S3M9F9 <alexandra_damgaard@swissre.com>
2024-12-10 11:51:42 +0200

    feat: add @EnableScheduling annotation

--- banking-system/banking-system-api/src/main/java/com/bankingsystem/api/Main.java
+++ banking-system/banking-system-api/src/main/java/com/bankingsystem/api/Main.java
@@ -4,10 +4,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = {"com.bankingsystem.api", "com.bankingsystem.domain", "com.bankingsystem.persistence"})
 @EntityScan(basePackages = "com.bankingsystem.persistence.model")
 @EnableJpaRepositories(basePackages = "com.bankingsystem.persistence.repository")
+@EnableScheduling
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
