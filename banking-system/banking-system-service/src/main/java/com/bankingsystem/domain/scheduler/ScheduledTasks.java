e3643b8c25d9f03e8235667ad1cd4dd2a04dc1b2
S3M9F9 <alexandra_damgaard@swissre.com>
2024-12-10 13:25:41 +0200

    feat: add initial delay of 1 minute when scheduling job every 2 minutes

--- banking-system/banking-system-service/src/main/java/com/bankingsystem/domain/scheduler/ScheduledTasks.java
+++ banking-system/banking-system-service/src/main/java/com/bankingsystem/domain/scheduler/ScheduledTasks.java
@@ -2,11 +2,16 @@ package com.bankingsystem.domain.scheduler;
 
 import com.bankingsystem.domain.service.CheckingAccountService;
 import com.bankingsystem.domain.service.JointCheckingAccountService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class ScheduledTasks {
+    private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
+
     private final CheckingAccountService checkingAccountService;
     private final JointCheckingAccountService jointCheckingAccountService;
 
@@ -16,9 +21,12 @@ public class ScheduledTasks {
     }
 
 //    @Scheduled(cron = "0 0 2 * * ?") // Run daily at 2 AM
-    @Scheduled(fixedRate = 120000) // 120,000 milliseconds = 2 minutes
+    @Scheduled(fixedRate = 120000, initialDelay = 60000) // Run every 2 minutes with initial delay of 1 minute
+    @Transactional
     public void applyInterestToOverdrawnAccountsTask() {
+        logger.info("Applying interest to overdrawn Checking Accounts");
         checkingAccountService.applyInterestToOverdrawnAccounts();
+        logger.info("Applying interest to overdrawn Joint Checking Accounts");
         jointCheckingAccountService.applyInterestToOverdrawnAccounts();
     }
 }
